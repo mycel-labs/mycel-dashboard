0f8e52646d42f80d1f6518171cb310f8f4b5c2c3
tarumi <taryunelab@gmail.com>
2023-07-25 20:28:34 +0200

    add isClaimable

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { SigningStargateClient, DeliverTxResponse } from "@cosmjs/stargate";
 import { OfflineDirectSigner } from "@keplr-wallet/types";
@@ -12,44 +12,69 @@ export default function Faucet() {
   const client = useClient();
 
   const [isShow, setIsShow] = useState<boolean>(false);
+  const [isClaimable, setIsClaimable] = useState<boolean>(false);
+  const [balance, setBalance] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [txResponse, setTxResponse] = useState<DeliverTxResponse>();
 
   const faucetMnemonic = import.meta.env.VITE_FAUCET_MNEMONIC ?? "";
 
-  const revceiveFaucet = async () => {
+  const queryBalance = async () => {
+    const balance = await client.CosmosBankV1Beta1.query.queryBalance(address, { denom: "mycel" }).then((res) => {
+      if (res.data.balance?.amount) {
+        return res.data.balance.amount;
+      }
+    });
+    setBalance(balance ?? "0");
+  };
+
+  useEffect(() => {
+    queryBalance();
+  }, [address]);
+
+  useEffect(() => {
+    if (parseInt(balance) < 1000) {
+      setIsClaimable(true);
+    } else {
+      setIsClaimable(false);
+    }
+  }, [balance]);
+
+  const claimFaucet = async () => {
     setIsLoading(true);
     setIsShow(true);
 
-    const faucetSigner = (await DirectSecp256k1HdWallet.fromMnemonic(faucetMnemonic, {
-      prefix: "mycel",
-    })) as OfflineDirectSigner;
-    const faucetAddress = (await faucetSigner.getAccounts())[0].address;
-    const rpc = import.meta.env.VITE_WS_TENDERMINT ?? "";
-    const amount = import.meta.env.VITE_FAUCET_AMOUNT ?? "1000";
-    const faucetClient = await SigningStargateClient.connectWithSigner(rpc, faucetSigner);
+    if (isClaimable) {
+      const faucetSigner = (await DirectSecp256k1HdWallet.fromMnemonic(faucetMnemonic, {
+        prefix: "mycel",
+      })) as OfflineDirectSigner;
+      const faucetAddress = (await faucetSigner.getAccounts())[0].address;
+      const rpc = import.meta.env.VITE_WS_TENDERMINT ?? "";
+      const amount = import.meta.env.VITE_FAUCET_AMOUNT ?? "1000";
+      const faucetClient = await SigningStargateClient.connectWithSigner(rpc, faucetSigner);
 
-    await faucetClient
-      .sendTokens(faucetAddress, address, [{ denom: "mycel", amount: amount }], {
-        amount: [{ denom: "mycel", amount: "500" }],
-        gas: "200000",
-      })
-      .then((res) => {
-        setIsLoading(false);
-        setTxResponse(res as DeliverTxResponse);
-      })
-      .catch((err) => {
-        setIsShow(false);
-        console.log(err);
-      });
+      await faucetClient
+        .sendTokens(faucetAddress, address, [{ denom: "mycel", amount: amount }], {
+          amount: [{ denom: "mycel", amount: "500" }],
+          gas: "200000",
+        })
+        .then((res) => {
+          setIsLoading(false);
+          setTxResponse(res as DeliverTxResponse);
+        })
+        .catch((err) => {
+          setIsShow(false);
+          console.log(err);
+        });
+    }
   };
 
   return (
     <>
-      <IgntButton className="w-full" onClick={revceiveFaucet}>
-        Receive
+      <IgntButton className="w-full" disabled={!isClaimable} onClick={claimFaucet}>
+        Claim
       </IgntButton>
-      <TxModal isShow={isShow} setIsShow={setIsShow} txResponse={txResponse} isLoading={isLoading} onClosed={} />
+      <TxModal isShow={isShow} setIsShow={setIsShow} txResponse={txResponse} isLoading={isLoading} />
     </>
   );
 }
