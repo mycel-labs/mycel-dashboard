33b55c7b762d7c139cfb410c0df5dc5f45a400ea
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-11-17 01:33:40 +0900

    update wallet modal

--- src/components/dialog/WalletDialog.tsx
+++ src/components/dialog/WalletDialog.tsx
@@ -2,15 +2,17 @@ import { Dialog } from "@headlessui/react";
 import useWallet from "@/hooks/useWallet";
 import { useStore } from "@/store/index";
 import Button from "@/components/Button";
-import { Unplug, Wallet, KeySquare } from "lucide-react";
+import { Unplug, Wallet, KeySquare, ClipboardCopy } from "lucide-react";
 import MetamaskIcon from "@/assets/icons/wallets/metamask.svg";
 import KeplrIcon from "@/assets/icons/wallets/keplr.svg";
+import MycelCharactor from "@/assets/mycel_charactor.svg";
 import { shortAddress } from "@/utils/wallets";
+import { copyClipboard } from "@/utils/lib";
 
 export default function WalletDialog() {
   const dialog = useStore((state) => state.dialog);
   const updateDialog = useStore((state) => state.updateDialog);
-  const { connectWallet, disconnectWallet, isConnected, evmAddress, deriveKeys } = useWallet();
+  const { connectWallet, disconnectWallet, isConnected, evmAddress, deriveKeys, mycelAccount } = useWallet();
 
   const DialogContent = () => (
     <div className="space-y-4 font-semibold">
@@ -52,18 +54,40 @@ export default function WalletDialog() {
   );
 
   const DialogContentConnected = () => (
-    <Button
-      className="btn-secondary w-full py-2"
-      onClick={async () => {
-        await disconnectWallet();
-        updateDialog(undefined);
-      }}
-    >
-      <span className="flex items-center justify-center px-6 mr-2">
-        <Unplug />
-        <span className="ml-4">Disconnect</span>
-      </span>
-    </Button>
+    <>
+      {evmAddress && (
+        <label className="relative">
+          EVM Address
+          <input type="text" readOnly value={shortAddress(evmAddress)} className="w-full" />
+          <div className="absolute right-0 bottom-0 h-12 w-12  flex items-center justify-center">
+            <button className="text-chocolat" onClick={() => copyClipboard(evmAddress)}>
+              <ClipboardCopy size={20} />
+            </button>
+          </div>
+        </label>
+      )}
+      <label className="relative">
+        Mycel Address
+        <input type="text" readOnly value={shortAddress(mycelAccount?.address)} className="w-full" />
+        <div className="absolute right-0 bottom-0 h-12 w-12  flex items-center justify-center">
+          <button className="text-chocolat" onClick={() => copyClipboard(mycelAccount?.address ?? "")}>
+            <ClipboardCopy size={20} />
+          </button>
+        </div>
+      </label>
+      <Button
+        className="btn-secondary w-full mt-8 py-2"
+        onClick={async () => {
+          await disconnectWallet();
+          updateDialog(undefined);
+        }}
+      >
+        <span className="flex items-center justify-center px-6 mr-2">
+          <Unplug />
+          <span className="ml-4">Disconnect</span>
+        </span>
+      </Button>
+    </>
   );
 
   const DialogContentKeyGen = () => (
@@ -93,9 +117,15 @@ export default function WalletDialog() {
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
       <div className="fixed inset-0 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end sm:items-center justify-center p-0 sm:p-4">
-          <Dialog.Panel className="mx-auto w-full sm:max-w-sm bg-cream py-12 px-10 rounded-t-md sm:rounded-md">
+          <Dialog.Panel className="mx-auto w-full sm:max-w-sm bg-cream py-12 px-10 rounded-t-lg sm:rounded-lg">
             <Dialog.Title className="text-2xl font-semibold mb-8 text-center">
-              {dialog === "wallet2" ? "Generate Mycel Account" : isConnected ? "Profile" : "Select Wallet"}
+              {dialog === "wallet2" ? (
+                "Generate Mycel Account"
+              ) : isConnected ? (
+                <img src={MycelCharactor} width={144} height={144} alt="Mycel" className="mx-auto" />
+              ) : (
+                "Select Wallet"
+              )}
             </Dialog.Title>
             {dialog === "wallet2" ? (
               <DialogContentKeyGen />
