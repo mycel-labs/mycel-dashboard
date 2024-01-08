c598b4c29057fa47fb5d49b02c837c2076cd408c
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-01-09 02:38:12 +0900

    fix

--- src/components/dialog/WalletDialog.tsx
+++ src/components/dialog/WalletDialog.tsx
@@ -1,23 +1,21 @@
-import { Dialog } from "@headlessui/react";
-import BaseDialog from "@/components/dialog/BaseDialog";
-import useBalances from "@/hooks/useBalances";
-import useWallet from "@/hooks/useWallet";
-import useWebAuthn from "@/hooks/useWebAuthn";
-import { useStore } from "@/store/index";
-import Button from "@/components/Button";
-import { Unplug, KeySquare, ClipboardCopy } from "lucide-react";
 import MycelCharactor from "@/assets/mycel_charactor.svg";
-import { shortAddress, WALLET_CONFIG, type WalletType } from "@/utils/wallets";
-import { copyClipboard, isPC, isMobile, cn, isOKXApp, isBitGetApp } from "@/utils/lib";
-import { MYCEL_COIN_DECIMALS, MYCEL_HUMAN_COIN_UNIT, MYCEL_BASE_COIN_UNIT, convertToDecimalString } from "@/utils/coin";
+import Button from "@/components/Button";
 import { toast } from "@/components/Toaster";
+import BaseDialog from "@/components/dialog/BaseDialog";
+import { useBalance } from "@/hooks/useMycel";
+import useWallet from "@/hooks/useWallet";
+import { useStore } from "@/store/index";
+import { MYCEL_COIN_DECIMALS, MYCEL_HUMAN_COIN_UNIT, convertToDecimalString } from "@/utils/coin";
+import { cn, copyClipboard, isBitGetApp, isMobile, isOKXApp, isPC } from "@/utils/lib";
+import { WALLET_CONFIG, type WalletType, shortAddress } from "@/utils/wallets";
+import { Dialog } from "@headlessui/react";
+import { ClipboardCopy, KeySquare, Unplug } from "lucide-react";
 
 export default function WalletDialog() {
   const dialog = useStore((state) => state.dialog);
   const updateDialog = useStore((state) => state.updateDialog);
   const { connectWallet, disconnectWallet, isConnected, evmAddress, deriveKeys, mycelAccount, connectorsWagmi } =
     useWallet();
-  const { createNewCredential } = useWebAuthn();
 
   const DialogContent = () => (
     <div className="space-y-4 font-semibold">
@@ -36,7 +34,7 @@ export default function WalletDialog() {
               } else if (isMobile()) {
                 window.open(`https://okex.com/web3/connect-dapp?uri=${encodeURIComponent(window.location.href)}`);
               } else if (isPC()) {
-                window.open(`https://www.okx.com/web3`);
+                window.open("https://www.okx.com/web3");
               }
             } else if (val.name === "BitGetWallet") {
               if (isBitGetApp()) {
@@ -44,10 +42,8 @@ export default function WalletDialog() {
               } else if (isMobile()) {
                 window.open(`https://bkcode.vip/?action=dapp&url=${encodeURIComponent(window.location.href)}`);
               } else {
-                window.open(`https://web3.bitget.com`);
+                window.open("https://web3.bitget.com");
               }
-            } else if (val.name === "Passkey") {
-              createNewCredential();
             } else {
               // TODO: refactor this
               if (
@@ -93,6 +89,7 @@ export default function WalletDialog() {
             <input type="text" readOnly value={shortAddress(evmAddress)} className="w-full" />
             <div className="absolute right-0 bottom-0 h-12 w-12  flex items-center justify-center">
               <button
+                type="button"
                 className="text-chocolat"
                 onClick={() => {
                   copyClipboard(evmAddress);
@@ -109,6 +106,7 @@ export default function WalletDialog() {
           <input type="text" readOnly value={shortAddress(mycelAccount?.address)} className="w-full" />
           <div className="absolute right-0 bottom-0 h-12 w-12 flex items-center justify-center">
             <button
+              type="button"
               className="text-chocolat"
               onClick={() => {
                 copyClipboard(mycelAccount?.address ?? "");
@@ -119,16 +117,15 @@ export default function WalletDialog() {
             </button>
           </div>
         </label>
-        <label htmlFor="balances">My Balance</label>
-        {!balances && <div className="text-gray-500">---</div>}
-        <ul>
-          {balances?.map((coin) => (
-            <li key={coin.denom} className="font-mono text-xl px-0.5">
-              {convertToDecimalString(coin.amount, MYCEL_COIN_DECIMALS)}
-              <span className="text-gray-600 ml-1 text-lg uppercase">{MYCEL_HUMAN_COIN_UNIT}</span>
-            </li>
-          ))}
-        </ul>
+        <label htmlFor="balance">My Balance</label>
+        {!dataBalance?.balance?.amount ? (
+          <div className="text-gray-500">---</div>
+        ) : (
+          <p className="font-mono text-xl px-0.5">
+            {convertToDecimalString(dataBalance.balance.amount, MYCEL_COIN_DECIMALS)}
+            <span className="text-gray-600 ml-1 text-lg uppercase">{MYCEL_HUMAN_COIN_UNIT}</span>
+          </p>
+        )}
         <Button
           className="btn-secondary w-full mt-8 h-12 rounded-md"
           onClick={async () => {
@@ -153,6 +150,7 @@ export default function WalletDialog() {
           <input type="text" readOnly value={shortAddress(evmAddress)} className="w-full" />
           <div className="absolute right-0 bottom-0 h-12 w-12 flex items-center justify-center">
             <button
+              type="button"
               className="text-chocolat"
               onClick={() => {
                 copyClipboard(evmAddress);
@@ -179,7 +177,7 @@ export default function WalletDialog() {
     </>
   );
 
-  const { balances } = useBalances();
+  const { isLoading: isLoadingBalance, data: dataBalance } = useBalance();
 
   return (
     <BaseDialog open={dialog === "wallet" || dialog === "wallet2"}>
